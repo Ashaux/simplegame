376cb0ae4a8baa7b6af91197d972fdf91fda0f75
Asha A <107199727+Ashaux@users.noreply.github.com>
2022-07-12 19:43:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ function jump(){
     },500);
 }
 
+var character =
+document.getElementById("character");
+var blocktwo = document.getElementById("blocktwo");
+function jump(){
+    if(character.classList != "animate"){
+    character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },400);
+}
+
 var checkDead = setInterval(function(){
     var characterTop =
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -22,5 +34,19 @@ var checkDead = setInterval(function(){
         alert("Game Over"); 
         }
 }, 10);
-         
+
+var checkDead = setInterval(function(){
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft =
+    parseInt(window.getComputedStyle(blocktwo).getPropertyValue("left"));
+
+    if (blockLeft<20 && blockLeft>0 && characterTop>=130){
+        block.style.animation ="none";
+        block.style.display ="none";
+        alert("Game Over"); 
+        }
+ 
+}, 10);
+       
 
